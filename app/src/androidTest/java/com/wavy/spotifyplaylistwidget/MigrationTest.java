e0fca33e9545ac9fe470ab7828571bd27672f3bf
Petteri Parikka <petteri.parikka@leonidasoy.fi>
2019-03-31 01:03:16 +0200

    Fix migration.

--- app/src/androidTest/java/com/wavy/spotifyplaylistwidget/MigrationTest.java
+++ app/src/androidTest/java/com/wavy/spotifyplaylistwidget/MigrationTest.java
@@ -10,14 +10,15 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 
 import androidx.room.testing.MigrationTestHelper;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import static com.wavy.spotifyplaylistwidget.db.AppDatabase.MIGRATION_1_2;
+
 @RunWith(AndroidJUnit4.class)
 public class MigrationTest {
-
-    // Nothing here yet, write some tests when migrations are added.
 
     private static final String TEST_DB = "migration-test";
 
@@ -32,7 +33,7 @@ public class MigrationTest {
 
     @Test
     public void migrate1To2() throws IOException {
-        // SupportSQLiteDatabase db = helper.createDatabase(TEST_DB, 1);
+        SupportSQLiteDatabase db = helper.createDatabase(TEST_DB, 1);
 
         // db has schema version 1. insert some data using SQL queries.
         // You cannot use DAO classes because they expect the latest schema.
@@ -44,7 +45,7 @@ public class MigrationTest {
 
         // Re-open the database with version 2 and provide
         // MIGRATION_1_2 as the migration process.
-        // db = helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2);
+         helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2);
 
         // MigrationTestHelper automatically verifies the schema changes,
         // but you need to validate that the data was migrated properly.
--- app/src/main/java/com/wavy/spotifyplaylistwidget/db/entity/WidgetEntity.java
+++ app/src/main/java/com/wavy/spotifyplaylistwidget/db/entity/WidgetEntity.java
@@ -4,6 +4,7 @@ import com.wavy.spotifyplaylistwidget.db.converter.InstantConverter;
 
 import org.threeten.bp.Instant;
 
+import androidx.annotation.NonNull;
 import androidx.room.Embedded;
 import androidx.room.Entity;
 import androidx.room.Index;
@@ -12,7 +13,6 @@ import androidx.room.TypeConverters;
 
 @Entity(tableName = "widgets",
         indices = {@Index(value = "androidWidgetId", name = "index_widgets_androidWidgetId")})
-
 public class WidgetEntity {
 
     @PrimaryKey
@@ -23,6 +23,7 @@ public class WidgetEntity {
     public Instant creationTime;
 
     @Embedded
+    @NonNull
     public WidgetOptions options;
 
     public WidgetEntity(int androidWidgetId, Instant creationTime, WidgetOptions options) {
